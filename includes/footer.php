ee8d1539789a8b5252c934176e91b6568a36ec8f
Saad <118393620+saad0010@users.noreply.github.com>
2023-08-22 02:44:55 +0500

    Basic Template

---
+++ includes/footer.php
@@ -0,0 +1,188 @@
+<!-- footer -->
+<footer class="bg-li py-5">
+	<div class="container py-xl-5 py-lg-3">
+		<!-- subscribe -->
+		<div class="subscribe mx-auto">
+			<div class="icon-effect-w3">
+				<span class="fa fa-envelope"></span>
+			</div>
+			<h2 class="tittle text-center font-weight-bold">Stay Connected !</h2>
+			<p class="sub-tittle text-center mt-3 mb-sm-5 mb-4">From Our Digital Marketing To Yours.</p>
+			<form action="#" method="post" class="subscribe-wthree pt-2">
+				<div class="d-flex subscribe-wthree-field">
+					<input class="form-control" type="email" placeholder="Enter your email..." name="email" required="">
+					<button class="btn form-control w-50" type="submit">Subscribe</button>
+				</div>
+			</form>
+		</div>
+		<!-- //subscribe -->
+	</div>
+</footer>
+<!-- //footer -->
+<!-- copyright bottom -->
+<!-- <div class="copy-bottom bg-li py-4 border-top">
+	<div class="container-fluid">
+		<div class="d-md-flex px-md-3 position-relative text-center">
+			 footer social icons 
+			<div class="social-icons-footer mb-md-0 mb-3">
+				<ul class="list-unstyled">
+					<li>
+						<a href="#">
+							<span class="fa fa-facebook"></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="fa fa-twitter"></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="fa fa-google-plus"></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="fa fa-instagram"></span>
+						</a>
+					</li>
+				</ul>
+			</div>
+			 //footer social icons 
+			 copyright 
+			<div class="copy_right mx-md-auto mb-md-0 mb-3">
+				<p class="text-bl let">© 2023 Digital Services. All rights reserved
+				</p>
+			</div>
+			 //copyright 
+			move top icon 
+			<a href="#home" class="move-top text-center">
+				<span class="fa fa-level-up" aria-hidden="true"></span>
+			</a>
+			 //move top icon 
+		</div>
+	</div>
+</div> -->
+<!-- //copyright bottom -->
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container-fluid">
+	<!-- Footer -->
+	<footer class="text-center text-lg-start text-dark" style="background-color: #ECEFF1">
+		<!-- Section: Social media -->
+		<section class="d-flex justify-content-between p-4 text-white" style="background-color: #21D192">
+			<!-- Left -->
+			<!-- <div class="me-5">
+                <span>Get connected with us on social networks:</span>
+            </div>
+            Left 
+
+            Right 
+            <div>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+
+            </div> -->
+			<!-- Right -->
+		</section>
+		<!-- Section: Social media -->
+
+		<!-- Section: Links  -->
+		<section class="">
+			<div class="container text-center text-md-start mt-5">
+				<!-- Grid row -->
+				<div class="row mt-3">
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+						<!-- Content -->
+						<h6 class="text-uppercase text-primary fw-bold text-left">Digital Solutions</h6>
+						<!-- <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" /> -->
+						<p class="mt-4 text-left text-dark">
+							Here you can use rows and columns to organize your footer
+							content. Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit.
+						</p>
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+						<!-- Links -->
+						<h6 class="text-uppercase text-primary fw-bold mb-4 text-left">Services</h6>
+						<!-- <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" /> -->
+						<p class="text-left">
+							<a href="webdesign.php" class="text-success">Website Design</a>
+						</p>
+						<p class="text-left">
+							<a href="dmarketing.php" class="text-success">Digital Marketing</a>
+						</p>
+						<p class="text-left">
+							<a href="webhost.php" class="text-success">Web Hosting </a>
+						</p>
+						<p class="text-left">
+							<a href="mobileapp.php" class="text-success">Mobile Applications</a>
+						</p>
+						<p class="text-left">
+							<a href="emboidery.php" class="text-success">Embroidery </a>
+						</p>
+						<p class="text-left">
+							<a href="raster.php" class="text-success">Rast To Vector</a>
+						</p>
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+						<!-- Links -->
+						<h6 class="text-uppercase text-primary fw-bold  mb-4 text-left">Useful links</h6>
+						<!-- <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" /> -->
+						<p class="text-left">
+							<a href="about.php" class="text-success ">About Us</a>
+						</p>
+						<p class="text-left">
+							<a href="pricing.php" class="text-success">Pricing</a>
+						</p>
+						<p class="text-left">
+							<a href="contact.php" class="text-success">Contact Us</a>
+						</p>
+						<p class="text-left">
+							<a href="faq.php" class="text-success">Help</a>
+						</p>
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+						<!-- Links -->
+						<h6 class="text-uppercase fw-bold text-primary mb-4 text-left">Contact</h6>
+						<!-- <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" /> -->
+						<h6 class="text-left text-dark mb-2"><i class="fa fa-location-arrow mr-3  "></i> New York, NY
+							10012, US
+						</h6>
+						<h5 class="text-left display-6 text-dark  mb-2"><i class="fa fa-envelope-o mr-3 "></i>
+							info@example.com</h5>
+						<h5 class="text-left text-dark mb-2"><i class="fa fa-phone-square mr-3 "></i> + 01 234 567 88
+						</h5>
+						<h5 class="text-left text-dark mb-2"><i class="fa fa-phone-square mr-3 "></i> + 01 234 567 89
+						</h5>
+					</div>
+					<!-- Grid column -->
+				</div>
+				<!-- Grid row -->
+			</div>
+		</section>
+		<!-- Section: Links  -->
+
+		<!-- Copyright -->
+		<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+			© 2023 Copyright:
+			<span class="text-dark">Digital Agency</span>
+		</div>
+		<!-- Copyright -->
+	</footer>
+	<!-- Footer -->
+</div>
+<!-- End of .container -->
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Website Design | Digital Marketing Agency</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="Digital Marketing Agency" />
+    <script>
+    addEventListener("load", function() {
+        setTimeout(hideURLbar, 0);
+    }, false);
+
+    function hideURLbar() {
+        window.scrollTo(0, 1);
+    }
+    </script>
+    <!-- //Meta tag Keywords -->
+
+    <!-- Custom-Files -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <!-- Style-CSS -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Font-Awesome-Icons-CSS -->
+    <!-- //Custom-Files -->
+
+    <!-- Web-Fonts -->
+    <link
+        href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=devanagari,latin-ext"
+        rel="stylesheet">
+    <!-- //Web-Fonts -->
+</head>
+
+<body>
+    <!-- main banner -->
+    <div class="main-top" id="home">
+        <!-- header -->
+        <header>
+            <div class="container-fluid">
+                <div class="header d-lg-flex justify-content-between align-items-center py-3 px-sm-3">
+                    <!-- logo -->
+                    <div id="logo">
+                        <h1><a href="index.php"><span class="fa fa-linode mr-2"></span>Digital Agency</a></h1>
+                    </div>
+                    <!-- //logo -->
+                    <!-- nav -->
+                    <div class="nav_w3ls">
+                        <nav>
+                            <label for="drop" class="toggle">Menu</label>
+                            <input type="checkbox" id="drop" />
+                            <ul class="menu">
+                                <!-- <li><a href="index.php" class="active">Home</a></li> -->
+                                <li><a href="about.php">About Us</a></li>
+
+                                <li>
+                                    <!-- First Tier Drop Down -->
+                                    <label for="drop-2" class="toggle toogle-2">Services <span class="fa fa-angle-down"
+                                            aria-hidden="true"></span>
+                                    </label>
+                                    <a href="#">Services <span class="fa fa-angle-down" aria-hidden="true"></span></a>
+                                    <input type="checkbox" id="drop-2" />
+                                    <ul>
+                                        <li><a href="webdesign.php" class="drop-text">Website Design</a></li>
+                                        <li><a href="dmarketing.php" class="drop-text">Digital Marketing</a></li>
+                                        <!-- <li><a href="404.php" class="drop-text">404</a></li> -->
+                                        <li><a href="webhost.php" class="drop-text">Web Hosting & Emails</a></li>
+                                        <li><a href="mobileapp.php" class="drop-text">Mobile Applications</a></li>
+                                        <li><a href="embroidery.php" class="drop-text">Emrbroidery Digitizing</a></li>
+                                        <li><a href="raster.php" class="drop-text">Raster To vector</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="pricing.php">Pricing</a></li>
+                                <li><a href="contact.php">Contact Us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!-- //nav -->
+                    <div class="d-flex mt-lg-1 mt-sm-2 mt-3 justify-content-center">
+                        <!-- search -->
+                        <!-- <div class="search-w3layouts mr-3">
+                            <form action="#" method="post" class="search-bottom-wthree d-flex">
+                                <input class="search" type="search" placeholder="Search Here..." required="">
+                                <button class="form-control btn" type="submit"><span
+                                        class="fa fa-search"></span></button>
+                            </form>
+                        </div> -->
+                        <!-- //search -->
+                        <!-- download -->
+                        <button class=" btn btn-success" onclick="window.location.href='contact.php';">
+                            Get In Touch <span class="fa fa-cloud-download"></span>
+                        </button>
+                        <!-- //download -->
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- //header -->
